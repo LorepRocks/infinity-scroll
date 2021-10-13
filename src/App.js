b904e22dae75541a871156484a01cb9f37fea6e3
loreprocks <lorenajrc@gmail.com>
2021-10-12 20:38:13 -0500

    first aproach for infinity scroll using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useCallback } from 'react'
+import useBookSearch from './useBookSearch'
+import './App.css'
+export default function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber)
 
-function App() {
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore){
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+
+    if(node) observer.current.observe(node)
+
+  }, [loading, hasMore])
+
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input type='text' value={query} onChange={handleChange}></input>
+      {books.map((book,index) => {
+        if ( books.length === index + 1) {
+          return <div className="offset" 
+            ref={lastBookElementRef} 
+            key={book+index}>
+              {book}
+            </div>
+        } else {
+          return <div className="offset" key={book+index}>{book}</div>
+        }
+      })}
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'There is an error'}</div>
+    </>
+  )
 }
-
-export default App;
